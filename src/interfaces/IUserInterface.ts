e26eb1740fdc88a2efc31eb432e926f9a8f32312
WWteodoro <bostawwtr@outlook.com>
2025-09-13 14:43:37 -0300

    feat: user entity and repository

---
+++ src/interfaces/IUserInterface.ts
@@ -0,0 +1,43 @@
+export interface IUser{
+    id: string;
+    name: string;
+    email: string;
+    password: string;    
+    createdAt?: Date;
+    updatedAt?: Date;
+    role?: string;
+}
+
+export interface IUserCreateRequest{
+    name: string;
+    email: string;
+    password: string;    
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface IUserGetRequest {
+    id: string;
+}
+
+export interface IUserGetByEmailRequest {
+    email: string;
+}
+
+export interface IUserUpdateRequest{
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    confirmEmail?: string;
+    confirmPassword?: string;
+}
+
+export interface IUserDeleteRequest{
+    id: string;
+}
+
+export interface IUserAuthenticateRequest {
+    email    : string;
+    password : string;
+}
---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,55 @@
+import { AppError } from "../errors/AppError";
+import { IUser } from "../interfaces/IUserInterface";
+import { IUserRepository } from "../interfaces/IUserRepository";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export class UserRepository implements IUserRepository{
+    constructor() {}
+    async findAll(): Promise<IUser[]> {
+        const result = await prisma.user.findMany()
+        return result
+    }
+
+    async insert(props: IUser): Promise<IUser> {
+        const result = await prisma.user.create({
+            data: {id: props.id,name: props.name, email: props.email, password: props.password}
+        })
+        
+        return result;
+
+    }
+
+    async findOneUser(id: string): Promise<IUser>{
+        const result = await prisma.user.findUnique({
+            where: { id }
+        })
+
+        if(!result) throw new AppError('User not found')
+        return result
+    }
+
+    async update(props: IUser, id: string): Promise<IUser> {
+        const result = await prisma.user.update({
+            where: { id },
+            data: {id: props.id,name: props.name, email: props.email, password: props.password},
+        })
+
+        return result;
+    };
+
+    async delete(id: string): Promise<void>{
+        await prisma.user.delete({
+            where: { id }
+        })
+    }
+
+    async findUserByEmail(Email: string): Promise<IUser> {
+        const result = await prisma.user.findUnique({
+          where: { name: Email }
+        });
+        if(!result) throw new Error('User not found')
+        
+        return result;
+      }
+}
